58665294ccd6ebcb998a94486e169c3fb50179e4
Khánh Vũ <khanh.vu4@tiki.vn>
2022-07-13 18:17:57 +0700

    feat/home: update reactions

--- js/home.js
+++ js/home.js
@@ -2,17 +2,30 @@ $(document).ready(() => {
   $(".owl-prefooter").owlCarousel({
     loop: true,
     autoplay: true,
-    autoWidth:true,
+    autoWidth: true,
     margin: 16,
-  });
+  })
 
   $(".owl-banner").owlCarousel({
     loop: true,
     autoplay: true,
-    autoWidth:true,
+    autoWidth: true,
     items: 1,
     center: true,
     dots: true,
     autoplayHoverPause: true,
+  })
+
+  const second = 1000;
+  const div = $("div.comments");
+  div.animate({
+    scrollTop: div.height(),
+  }, 30 * second)
+  div.mouseenter(() => {
+    div.stop();
+  }).mouseleave(() => {
+    div.animate({
+      scrollTop: div.height(),
+    }, 30 * second)
   })
 })
