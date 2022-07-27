0cf2fb0f93e0a8eb7271d64fc94c1054a53ab665
Khánh Vũ <khanh.vu4@tiki.vn>
2022-07-27 16:44:43 +0700

    feat/index: fix bug

--- js/product-detail.js
+++ js/product-detail.js
@@ -1,6 +1,15 @@
 $(document).ready(() => {
   const $productSlide = $(".product-carousel")
-  const $productDots = $(".product-dots")
+  const $productDots = $(".product-dots .owl-carousel")
+  const $remain = $(".dots-remain")
+
+  function countItem(index, count) {
+    if (index - 4 > 0) {
+      $remain.text("+" + (index - 4))
+    } else {
+      $remain.text("+" + (count - index - 4))
+    }
+  }
 
   $productSlide
     .owlCarousel({
@@ -10,6 +19,9 @@ $(document).ready(() => {
       items: 1,
       center: true,
       dots: true,
+      onInitialized: function (e) {
+        countItem(e.item.index, e.item.count)
+      },
     })
     .on("dragged.owl.carousel", function (e) {
       if (e.relatedTarget["_drag"]["direction"] == "left") {
@@ -18,18 +30,35 @@ $(document).ready(() => {
         $productDots.trigger("prev.owl.carousel")
       }
     })
+    .on("changed.owl.carousel", function (e) {
+      const index = e.item.index
+      countItem(index, e.item.count)
+    })
 
   $productDots
     .owlCarousel({
       loop: false,
       autoplay: false,
-      autoWidth: false,
+      autoWidth: true,
       items: 4,
     })
     .on("changed.owl.carousel", function (e) {
       const currentIndex = e.relatedTarget["_current"]
       $productSlide.trigger("to.owl.carousel", currentIndex)
+
+      var current = e.item.index + 1
+      total = e.item.count
+      if (total - current < 4) {
+        $remain.hide()
+      } else {
+        $remain.show()
+      }
     })
 
-    $('.chocolat-parent').Chocolat();
+  $productDots.on("click", ".owl-item", function (e) {
+    const index = $(this).index()
+    $productSlide.trigger("to.owl.carousel", index)
+  })
+
+  $(".chocolat-parent").Chocolat()
 })
