b54e644240a67db9b82bc57771b68c4584e8e427
Khánh Vũ <khanh.vu4@tiki.vn>
2022-07-29 11:44:27 +0700

    feat/product-detail: fix slide

--- js/product-detail.js
+++ js/product-detail.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+  const $main = $(".main-image")
   const $mainImage = $(".main-image img")
+  const $mainVideo = $(".main-image iframe")
   const $slide = $(".fake-slide")
   const $item = $slide.find(".item")
   const $remain = $(".remain")
@@ -11,13 +13,51 @@ $(document).ready(function () {
     $remain.text("+" + delta)
   }
 
-  $mainImage.attr("src", $slide.find("img:first").attr("src"))
+  $remain.click(function () {
+    $item.first().click()
+  })
+
+  // set default main image
+  const firstType = $item.first().children().prop("tagName").toLowerCase()
+  const firstSrc = $item.first().children().attr("src")
+  if (firstType === "img") {
+    $mainImage.show()
+    $mainImage.attr("src", firstSrc)
+    $mainVideo.hide()
+  } else {
+    $mainVideo.show()
+    $mainVideo.attr("src", firstSrc)
+    $mainImage.hide()
+  }
+  // set default main image
 
   $item.each(function () {
     const $this = $(this)
+    const itemType = $this.children().first().prop("tagName").toLowerCase()
 
+    // block user from play video in fake slide
+    if (itemType !== "img") {
+      $this.addClass("has-video")
+    }
+    // block user from play video in fake slide
+
+    // update main slide image on hover
     $this.on("mouseover", function () {
-      $mainImage.attr("src", $(this).find("img").attr("src"))
+      const src = $(this).children().first().attr("src")
+      $main.attr("href", src)
+      if (itemType === "img") {
+        $mainImage.show()
+        $mainImage.attr("src", src)
+        $mainVideo.hide()
+      } else {
+        $mainVideo.show()
+        $mainVideo.attr("src", src)
+        $mainImage.hide()
+      }
     })
+    // update main slide image on hover
   })
+
+  $(".item").flashy()
+  $main.flashy()
 })
