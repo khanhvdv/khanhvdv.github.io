2a221c5fd71717572ec96f324951a5afadeee078
Khánh Vũ <khanh.vu4@tiki.vn>
2022-07-29 13:50:57 +0700

    feat/index: fix bug

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   $(".owl-prefooter").infiniteslide({
     direction: "right",
-    pauseonhover: false
+    pauseonhover: false,
   })
 
   $(".owl-banner").owlCarousel({
@@ -18,15 +18,34 @@ $(document).ready(() => {
 
   new Swiper(".swiper", {
     direction: "vertical",
+    autoHeight: true,
     pagination: {
       el: ".custom-swiper-pagination",
       clickable: true,
     },
+    spaceBetween: 23,
     speed: 3000,
     autoplay: {
-      delay: 50000,
+      delay: 3000,
     },
     slidesPerView: 1,
     loop: true,
+    on: {
+      init: function () {
+        setSlideHeight(this)
+      },
+      slideChangeTransitionEnd: function () {
+        setSlideHeight(this)
+      },
+    },
   })
+
+  function setSlideHeight(that) {
+    $(".swiper-slide").css({ height: "auto" })
+    var currentSlide = that.activeIndex
+    var newHeight = $(that.slides[currentSlide]).height()
+
+    $(".swiper-wrapper,.swiper-slide").css({ height: newHeight })
+    that.update()
+  }
 })
